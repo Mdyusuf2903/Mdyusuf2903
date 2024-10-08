7d44876bea22fedee757c9cc21b4a7df0ac606e2
Mohamed_Yusuf <91239520+Mdyusuf2903@users.noreply.github.com>
2024-10-08 14:20:30 +0530

    Update UserRepository.java

--- spring-boot-crud/src/main/java/com/yusuf/springbootdeveloper/repository/UserRepository.java
+++ spring-boot-crud/src/main/java/com/yusuf/springbootdeveloper/repository/UserRepository.java
@@ -4,7 +4,7 @@ package com.yusuf.springbootdeveloper.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.yusuf.springbootdeveloper.model.User;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     // No additional queries needed, JpaRepository provides CRUD operations
 }
